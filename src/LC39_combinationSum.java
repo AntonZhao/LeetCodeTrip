9c1ada6e4b9ee2110932de16dcc52082363c82bb
AntonZhao <519102870@qq.com>
2020-09-10 09:19:29 +0800

    test ci = commit

--- src/LC39_combinationSum.java
+++ src/LC39_combinationSum.java
@@ -1,37 +1,38 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class LC39_combinationSum {
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(0, target, new ArrayList<>(), candidates, res);
+        dfs(0, new LinkedList<>(), target, candidates, res);
         return res;
     }
 
-    private void backtrack(int depth, int target, ArrayList<Integer> path, int[] candidates, List<List<Integer>> res) {
+    private void dfs(int depth, LinkedList<Integer> curr, int target, int[] candidates, List<List<Integer>> res) {
         if (target < 0) {
             return;
         }
-        if (target == 0) {
-            res.add(new ArrayList<>(path));
-            System.out.println(path);
+        if (0 == target) {
+            res.add(new ArrayList<>(curr));
             return;
         }
-
         for (int i = depth; i < candidates.length; i++) {
-            path.add(candidates[i]);
-            backtrack(i, target - candidates[i], path, candidates, res);
-            // not i + 1 because we can reuse same elements
-            path.remove(path.size() - 1);
+            curr.add(candidates[i]);
+            dfs(i, curr, target - candidates[i], candidates, res);
+            curr.removeLast();
         }
+
     }
+
 
     public static void main(String[] args) {
         LC39_combinationSum ll = new LC39_combinationSum();
 
         int[] cc = {2, 3, 6, 7};
-        ll.combinationSum(cc, 7);
+        List<List<Integer>> lists = ll.combinationSum(cc, 7);
+        for (List<Integer> list : lists) {
+            System.out.println(list.toString());
+        }
     }
 }
